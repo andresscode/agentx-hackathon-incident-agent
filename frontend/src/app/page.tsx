debc99a586108f35de64a68f88921f720bdf93e1
Andres Martinez <21141606+andresscode@users.noreply.github.com>
2026-04-07 23:13:41 -0500

    style: configure biome and format code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function HomePage() {
-  return <div className="min-h-svh flex justify-center items-center">
-<h1 className="text-4xl font-bold">Hello, World!</h1>
+  return (
+    <div className="min-h-svh flex justify-center items-center">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
     </div>
+  )
 }
